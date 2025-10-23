b9aaaaa272eb0aac65fa8e46703814f556b79d47
sekarasriii <sekareeesh@gmail.com>
2025-10-23 15:47:13 +0700

    menambahkan parameter modifier untuk fungsi pertama

--- app/src/main/java/com/example/meet4_contoh/terserah.kt
+++ app/src/main/java/com/example/meet4_contoh/terserah.kt
@@ -16,4 +16,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun AktivitasPertama(modifier: Modifier = Modifier) {}
+fun AktivitasPertama(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(top = 100.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+}
