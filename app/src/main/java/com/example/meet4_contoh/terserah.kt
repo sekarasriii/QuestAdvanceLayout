2985818e461399f9b243cea2917135faf4257e90
sekarasriii <sekareeesh@gmail.com>
2025-10-23 15:49:58 +0700

    menambahkan spacer antar teks

--- app/src/main/java/com/example/meet4_contoh/terserah.kt
+++ app/src/main/java/com/example/meet4_contoh/terserah.kt
@@ -32,4 +32,5 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
             text = stringResource(id = R.string.univ),
             fontSize = 22.sp
         )
+        Spacer(modifier = Modifier.height(25.dp))
 }
