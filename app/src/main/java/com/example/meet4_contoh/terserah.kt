9eac31287a88cf9b0e324930a6a2087893b57876
sekarasriii <sekareeesh@gmail.com>
2025-10-23 16:15:45 +0700

    menambahkan isi text dengan string alamat

--- app/src/main/java/com/example/meet4_contoh/terserah.kt
+++ app/src/main/java/com/example/meet4_contoh/terserah.kt
@@ -63,5 +63,11 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
                         color = Color.White,
                         modifier = Modifier.padding(top = 15.dp)
                     )
+                    Text(
+                        text = stringResource(id = R.string.alamat),
+                        fontSize = 20.sp,
+                        color = Color.Yellow,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
         }
 }
